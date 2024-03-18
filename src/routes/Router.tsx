ef98d89a8dcf6f0bf2c023593b2f0a86c771fe8c
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-03-18 18:05:58 +0530

    feat(routes): Added user routes
    Routes Added:
    
    - Events
    - Slots
    - Profile

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import useUserLoginStatus from "../hooks/useUserLoginStatus";
 import Dashboard from "../pages/Admin/Dashboard/Dashboard";
-import EventSlots from "../pages/Admin/EventSlots/EventSlots";
-import Events from "../pages/Admin/Events/Events";
+import AdminEventSlots from "../pages/Admin/EventSlots/EventSlots";
+import AdminEvents from "../pages/Admin/Events/Events";
 import Registerations from "../pages/Admin/Registerations/Registerations";
 import UserRegisterations from "../pages/Admin/UserRegisterations/UserRegisterations";
 import Users from "../pages/Admin/Users/Users";
+import Events from "../pages/Events/Events";
 import Home from "../pages/Home/Home";
 import NotFound from "../pages/NotFound/NotFound";
+import Profile from "../pages/Profile/Profile";
+import Slots from "../pages/Slots/Slots";
 import UserLogin from "../pages/UserLogin/UserLogin";
 import UserRegister from "../pages/UserRegister/UserRegister";
 import AdminRoutes from "./conditionalRoutes/AdminRoutes";
@@ -33,16 +36,18 @@ const Router = () => {
         </Route>
 
         {/* User Routes */}
-        <Route path="/user" element={<UserRoutes userData={userData} />}>
-          <Route path="" Component={Home} />
+        <Route element={<UserRoutes userData={userData} />}>
+          <Route path="/events" Component={Events} />
+          <Route path="/events/:eventId/slots" Component={Slots} />
+          <Route path="/profile" Component={Profile} />
         </Route>
       </Route>
 
       {/* Admin Routes */}
       <Route path="/admin" element={<AdminRoutes userData={userData} />}>
         <Route path="" Component={Dashboard} />
-        <Route path="events" Component={Events} />
-        <Route path="events/:eventId/slots" Component={EventSlots} />
+        <Route path="events" Component={AdminEvents} />
+        <Route path="events/:eventId/slots" Component={AdminEventSlots} />
         <Route path="registerations" Component={Registerations} />
         <Route path="users" Component={Users} />
         <Route
