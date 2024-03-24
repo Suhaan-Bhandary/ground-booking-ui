d1ad6b76f1e92e36017163f289ed88409f4ca220
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-03-24 12:38:20 +0530

    feat: created basic admin page layout

--- src/pages/Admin/Events/Events.tsx
+++ src/pages/Admin/Events/Events.tsx
@@ -1,5 +1,34 @@
+import Table from "../../../components/Table/Table";
+
 const Events = () => {
-  return <div>Events</div>;
+  return (
+    <div>
+      <div className="container">
+        <div>
+          <h1>Events</h1>
+          <button>Create Event</button>
+        </div>
+
+        <div>
+          <Table>
+            <thead>
+              <tr>
+                <th>Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Event 1</td>
+              </tr>
+              <tr>
+                <td>Event 2</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Events;
