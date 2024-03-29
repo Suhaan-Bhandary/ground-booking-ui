901ffe591abcd89f4ca9a0b8dcbda9297601a031
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-03-29 23:15:00 +0530

    feat: added payment page

---
+++ src/app/features/registrationApi.ts
@@ -0,0 +1,30 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { API_BASE_URL } from "../../constants/app";
+import { getUserAuthToken } from "../../helpers/authToken";
+
+export const registrationApi = createApi({
+  reducerPath: "registrationApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${API_BASE_URL}/registrations`,
+  }),
+  tagTypes: ["Registration"],
+  endpoints: (builder) => ({
+    payment: builder.mutation<void, { registrationId: number; amount: number }>(
+      {
+        query: (data) => ({
+          url: `/${data.registrationId}/payments`,
+          method: "POST",
+          body: {
+            payment: {
+              amount: data.amount,
+              status: "PAID",
+            },
+          },
+          headers: { Authorization: `Bearer ${getUserAuthToken()}` },
+        }),
+      },
+    ),
+  }),
+});
+
+export const { usePaymentMutation } = registrationApi;
