aeed77ae6d8136a7d54da3377665b9e0bf35a33b
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-03-30 10:56:12 +0530

    feat: header styling and login screen

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,50 +1,82 @@
+import { useState } from "react";
+import { MdOutlineClose } from "react-icons/md";
+import { RxHamburgerMenu } from "react-icons/rx";
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../../hooks/redux";
 import Logout from "../Logout/Logout";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   const userData = useAppSelector((state) => state.userState.user);
   const isLoggedIn = userData ? true : false;
 
-  return (
-    <header className={styles.headerWrapper}>
-      <div className={`container ${styles.Header}`}>
-        <Link to="/">
-          <h1 className={styles.title}>Ground Booking</h1>
-        </Link>
+  const toggleNav = () => {
+    setIsNavOpen((state) => !state);
+  };
 
-        <nav>
-          <ul className={styles.navLinks}>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/events">Events</Link>
-            </li>
-            {isLoggedIn ? (
-              <>
-                <li>
-                  <Link to="/profile">{userData?.user_name}</Link>
-                </li>
-                <li>
-                  <Logout>Logout</Logout>
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-                <li>
-                  <Link to="/register">Register</Link>
-                </li>
-              </>
-            )}
-          </ul>
-        </nav>
+  return (
+    <>
+      <header className={styles.headerWrapper}>
+        <div className={`container ${styles.Header}`}>
+          <Link to="/">
+            <h1 className={styles.title}>Ground Booking</h1>
+          </Link>
+
+          <button
+            type="button"
+            onClick={toggleNav}
+            className={styles.mobileNavToggle}
+            aria-controls="primary-navigation"
+          >
+            {!isNavOpen ? <RxHamburgerMenu /> : <MdOutlineClose />}
+            <span className="visually-hidden">Menu</span>
+          </button>
+
+          <nav className={`${styles.nav} ${isNavOpen ? styles.navOpen : ""}`}>
+            <ul className={styles.navLinks}>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/events">Events</Link>
+              </li>
+              {isLoggedIn ? (
+                <>
+                  <li>
+                    <Link to="/profile">{userData?.user_name}</Link>
+                  </li>
+                  <li>
+                    <Logout className={styles.button}>Logout</Logout>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <Link to="/login" className={styles.button}>
+                      Login
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/register" className={styles.button}>
+                      Register
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      {/* Div is used to provide backdrop shadow */}
+      <div
+        className={`${styles.backdrop} ${isNavOpen ? styles.backdropOpen : ""}`}
+      >
+        {" "}
       </div>
-    </header>
+    </>
   );
 };
 
