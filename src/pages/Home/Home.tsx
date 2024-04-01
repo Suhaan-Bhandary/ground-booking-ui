b07a743ccb1171a3c0bab982be23d9e85cb06c98
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-04-01 10:48:51 +0530

    feat: created banner for home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,26 @@
 import { Link } from "react-router-dom";
+import sportsImage from "../../assets/sports.png";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <main>
-      <section>
-        <h1>Ground Booking App</h1>
-        <p>Ground Booking</p>
-        <Link to="/events">Events</Link>
+    <main className="container">
+      <section className={styles.banner}>
+        <div className={styles.containerWrapper}>
+          <div className={styles.bannerContent}>
+            <h1 className={styles.bannerTitle}>Ground Booking App</h1>
+            <p className={styles.bannerDetail}>
+              Book ground to play cricket, badminton and other games with your
+              friends in just one click!!
+            </p>
+            <Link to="/events" className={styles.bannerButton}>
+              Events
+            </Link>
+          </div>
+          <div className={styles.bannerImage}>
+            <img src={sportsImage} alt="sports" />
+          </div>
+        </div>
       </section>
 
       <section>
