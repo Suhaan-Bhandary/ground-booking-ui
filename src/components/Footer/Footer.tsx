e16519da6cd317336e2c8599e140869a19ebad18
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-04-01 11:05:29 +0530

    feat: added footer to home page

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <section className={styles.footer}>
+        <div className="container">
+          <div className={styles.footerContainer}>
+            <div className={styles.info}>
+              <h2>Ground booking</h2>
+              <p>
+                Book ground to play cricket, badminton and other games with your
+                friends in just one click!!
+              </p>
+            </div>
+
+            <div className={styles.links}>
+              <h2>Links</h2>
+              <Link to="/credits">Credits</Link>
+              <a href="mailto:ground-booking@gmail.com">Contact Us</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.copyRight}>
+        <div className="container">
+          <p>Ground booking</p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,36 +1,33 @@
 import { Link } from "react-router-dom";
 import sportsImage from "../../assets/sports.png";
 import styles from "./Home.module.css";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
-    <main className="container">
-      <section className={styles.banner}>
-        <div className={styles.containerWrapper}>
-          <div className={styles.bannerContent}>
-            <h1 className={styles.bannerTitle}>Ground Booking App</h1>
-            <p className={styles.bannerDetail}>
-              Book ground to play cricket, badminton and other games with your
-              friends in just one click!!
-            </p>
-            <Link to="/events" className={styles.bannerButton}>
-              Events
-            </Link>
+    <div className="full-height">
+      <main className="container">
+        <section className={styles.banner}>
+          <div className={styles.containerWrapper}>
+            <div className={styles.bannerContent}>
+              <h1 className={styles.bannerTitle}>Ground Booking App</h1>
+              <p className={styles.bannerDetail}>
+                Book ground to play cricket, badminton and other games with your
+                friends in just one click!!
+              </p>
+              <Link to="/events" className={styles.bannerButton}>
+                Events
+              </Link>
+            </div>
+            <div className={styles.bannerImage}>
+              <img src={sportsImage} alt="sports" />
+            </div>
           </div>
-          <div className={styles.bannerImage}>
-            <img src={sportsImage} alt="sports" />
-          </div>
-        </div>
-      </section>
+        </section>
+      </main>
 
-      <section>
-        <h2>Features</h2>
-      </section>
-
-      <section>
-        <h2>Footer</h2>
-      </section>
-    </main>
+      <Footer />
+    </div>
   );
 };
 
