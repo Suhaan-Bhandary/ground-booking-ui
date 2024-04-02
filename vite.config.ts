0120772ae43a02d78ad370bbbd1e3eceff9f5efb
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-04-02 17:43:19 +0530

    feat: added manifest file and favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,32 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        short_name: "Ground Booking",
+        name: "Ground Booking",
+        icons: [
+          {
+            src: "favicon/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "favicon/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+
   server: {
     port: 3000,
   },
