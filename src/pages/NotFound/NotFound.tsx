9e5f95caf15024c89f84acad497b1ddd1bbc5489
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-04-04 12:40:45 +0530

    feat: created user not found page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+import styles from "./NotFound.module.css";
+
 const NotFound = () => {
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className={styles.NotFound}>
+      <h1>Page Not Found</h1>
+      <p>
+        Page you are looking is not available!!, Use the below link to head to
+        Home Page.
+      </p>
+      <Link to="/" className={styles.homePageLink}>
+        Home Page
+      </Link>
     </div>
   );
 };
